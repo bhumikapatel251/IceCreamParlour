86cb470e02b7729cd11ad815b7cbdb464e298f92
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-09-08 12:24:34 +0530

    Initial Commit

---
+++ IceCreamParlour/IceCreamParlourApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceCreamParlourApp.swift
+//  IceCreamParlour
+//
+//  Created by Bhumika Patel on 08/09/22.
+//
+
+import SwiftUI
+
+@main
+struct IceCreamParlourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
